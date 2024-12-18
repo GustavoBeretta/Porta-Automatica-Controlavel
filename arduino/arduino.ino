ca495f9cc693cc1b2cddce39f35ff4a5d4f6a2ae
Gustavo Beretta <gustavoberettag@gmail.com>
2024-12-18 12:18:20 -0300

    arduino update

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,27 +1,29 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
 #include <Servo.h>
-#include <SoftwareSerial.h>
 
 const int trigPin = 7;
 const int echoPin = 8;
 Servo servoMotor;
 
-SoftwareSerial espSerial(0, 1);
+const char* ssid = "S22+ de Gustavo";
+const char* password = "12345678";
+const String serverURL = "http://192.168.0.160:8000/logging";
 
 void setup() {
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   servoMotor.attach(9);
   servoMotor.write(0);
-  Serial.begin(9600);
-  espSerial.begin(115200);
+  Serial.begin(115200);
 
-  sendCommand("AT", 2000);
-
-  String ssid = "S22+ de Gustavo"; 
-  String password = "12345678";
-  sendCommand("AT+CWJAP=\"" + ssid + "\",\"" + password + "\"", 5000);
-
-  checkWiFiConnection();
+  Serial.print("Conectando-se ao WiFi");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nConectado ao WiFi.");
 }
 
 void loop() {
@@ -41,56 +43,30 @@ void loop() {
 
   if (distancia <= distanciaLimite) {
     servoMotor.write(90);
-    sendDistanceToBackend(distancia);
+    if (WiFi.status() == WL_CONNECTED) {
+      HTTPClient http;
+
+      http.begin(serverURL);
+      http.addHeader("Content-Type", "application/json");
+
+      String payload = "{\"distancia\": " + String(distancia) + "}";
+      int httpResponseCode = http.POST(payload);
+
+      if (httpResponseCode > 0) {
+        Serial.print("Resposta do servidor: ");
+        Serial.println(httpResponseCode);
+      } else {
+        Serial.print("Erro ao enviar os dados: ");
+        Serial.println(http.errorToString(httpResponseCode).c_str());
+      }
+
+      http.end();
+    } else {
+      Serial.println("Falha na conexão WiFi.");
+    }
   } else if (distancia < 440) {
     servoMotor.write(0);
   }
 
   delay(500);
-}
-
-void sendCommand(String command, const int timeout) {
-  String response = "";
-  espSerial.println(command);
-  unsigned long previousMillis = millis();
-  
-  while (millis() - previousMillis < timeout) {
-    while (espSerial.available()) {
-      char c = espSerial.read();
-      response += c;
-    }
-  }
-  
-  Serial.println(response);
-}
-
-void checkWiFiConnection() {
-  // Verifica a conexão Wi-Fi
-  sendCommand("AT+CWJAP?", 5000);  // Envia o comando para verificar se o ESP está conectado à rede Wi-Fi
-  sendCommand("AT+CIPSTATUS", 5000);  // Verifica o status da conexão de rede
-  Serial.print("Estou aqui0");
-}
-
-void sendDistanceToBackend(int distancia) {
-  String postData = "{\"distancia\": \"" + String(distancia) + "\"}";
-  String url = "http://192.168.194.137:8000/controle";
-
-  Serial.print("Estou aqui");
-
-  sendCommand("AT+CIPSTART=\"TCP\",\"" + url + "\",80", 3000);
-  delay(1000);
-
-  Serial.print("Estou aqui2");
-  
-  String request = "POST /controle HTTP/1.1\r\n";
-  request += "Host: IP_DO_SEU_SERVIDOR\r\n";
-  request += "Content-Type: application/json\r\n";
-  request += "Content-Length: " + String(postData.length()) + "\r\n";
-  request += "\r\n";
-  request += postData;
-
-  sendCommand("AT+CIPSEND=" + String(request.length()), 2000);  // Enviar o tamanho da requisição
-  sendCommand(request, 2000);  // Enviar a requisição POST
-
-  Serial.print("Estou aqui3");
 }
